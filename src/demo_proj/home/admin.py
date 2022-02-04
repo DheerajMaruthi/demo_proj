80a923cf6fbf1152158ee05be5b9b2fe3b14e9e0
Your Name <you@example.com>
2022-02-04 21:53:04 +0530

    [2022-02-04 21:53:04] form errors

--- src/demo_proj/home/admin.py
+++ src/demo_proj/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import HomePage
 # Register your models here.
+
+#python manage.py createsuperuser
+
+# admin.site.register(HomePage)
+
+@admin.register(HomePage)
+class HomePageModelAdmin(admin.ModelAdmin):
+    list_display = ['name','city']
